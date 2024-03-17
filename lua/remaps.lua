9f970d7dc6c08c3cba8329c73e6ff3c569f1a875
sceredi <ceredi.simone.iti@gmail.com>
2024-03-17 14:55:53 +0100

    feat: open terminal shortcut

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -35,4 +35,4 @@ vim.keymap.set('n', '<leader>rw', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left>
 
 vim.keymap.set('n', '<left>', '<cmd>tabprev<CR>', { desc = 'Go to previous tab' })
 vim.keymap.set('n', '<right>', '<cmd>tabnext<CR>', { desc = 'Go to next tab' })
-vim.keymap.set('n', '<leader>tt', '<cmd>tabnew | term<CR> | <cmd>file Term<CR>', { desc = 'Open new tab' })
+vim.keymap.set('n', '<leader>tt', '<cmd>tabnew | term<CR>', { desc = 'Open new tab' })
