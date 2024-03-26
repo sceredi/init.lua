5a6aca5822b64ff6729a7ce295f87f8ed4b8848f
sceredi <ceredi.simone.iti@gmail.com>
2024-03-26 22:46:55 +0100

    feat: code lenses are useful in ocaml

--- ftplugin/ocaml.lua
+++ ftplugin/ocaml.lua
@@ -1,1 +1,9 @@
 vim.opt_local.shiftwidth = 2
+
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+  desc = 'Refresh code lens on save',
+  group = vim.api.nvim_create_augroup('ocaml_codelens_custom', { clear = true }),
+  callback = function()
+    vim.lsp.codelens.refresh()
+  end,
+})
