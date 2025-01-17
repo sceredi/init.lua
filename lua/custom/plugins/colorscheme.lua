ca1e8c23f5c37f3095b0ed1c271ee65cfe7e39fa
sceredi <ceredi.simone.iti@gmail.com>
2025-01-17 17:13:42 +0100

    chore: wip

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -7,16 +7,4 @@ return {
       vim.cmd.colorscheme 'rose-pine-moon'
     end,
   },
-
-  -- {
-  --   'zenbones-theme/zenbones.nvim',
-  --   dependencies = 'rktjmp/lush.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function()
-  --     vim.o.background = 'light'
-  --     vim.cmd.colorscheme 'zenbones'
-  --   end,
-  -- },
 }
